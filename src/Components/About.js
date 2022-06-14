0b5641bc8d32b936e67cfd50e23dc98ec12c38b4
JC-MT <jancarlosmatias@gmail.com>
2022-06-14 09:25:56 -0400

    adds fetch and state to Home.js file

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,7 @@
 const About = () => {
     return (
       <div>
-          <h1>You made it to our About Page</h1>
+          <h1>About</h1>
       </div>
     );
   };
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,40 @@
-const Home = () => {
+import { Component } from 'react';
+
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+  }
+
+  updateSearch = (event) => {
+    const { value } = event.target 
+    this.setState({ search: value })
+  }
+
+  submitSearch = () => {
+    const KEY = 'AIzaSyDW_6OhDDkoDL23vieUpnfIVMHn45ga4Sw'
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.state.search}=video&key=${KEY}`)
+    .then((response) => {
+        console.log(this.state.search)
+        this.setState({ search: '' })
+        return response.json();
+      })
+      .then(console.log)
+      .catch(console.log);
+  }
+
+  render() {
     return (
       <div>
-          <h1>Yo Home</h1>
+        <h1>Home</h1>
+        <input onChange={this.updateSearch} value={this.state.search} placeholder="Search..." type="text"></input>
+        <button onClick={this.submitSearch}>Search</button>
       </div>
     );
-  };
-  
-  export default Home;
+  }
+}
+
+export default Home;
