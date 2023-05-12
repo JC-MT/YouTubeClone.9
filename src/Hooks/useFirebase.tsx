86f6ed05160d0528b4cfa91f4bea7695e0a519a2
Jan Matias <jancarlosmatias@gmail.com>
2023-05-12 12:47:37 -0400

    Adds types to useFirebase hook

--- src/Hooks/useFirebase.tsx
+++ src/Hooks/useFirebase.tsx
@@ -1,33 +1,45 @@
 import { db } from '../Firebase';
+import { useState, useEffect } from 'react';
 import {
   collection,
   addDoc,
   query,
   where,
   onSnapshot,
+  CollectionReference,
+  QuerySnapshot,
+  Query,
+  DocumentData
 } from 'firebase/firestore';
 
-import { useState, useEffect } from 'react';
-export default function useFirebase(videoId){
-  const [comments, setComments] = useState([]);
-  const commentsCollection = collection(db, 'comments');
-  const q = query(commentsCollection, where('videoId', '==', videoId));
+export default function useFirebase(videoId: string) {
+  const [comments, setComments]: [Object[], Function] = useState([]);
+  const commentsCollection: CollectionReference = collection(db, 'comments');
+  const q: Query<DocumentData> = query(
+    commentsCollection,
+    where('videoId', '==', videoId)
+  );
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      const allComments = [];
-      snapshot.forEach((doc) => {
-        allComments.push(doc.data());
-      });
-      setComments(allComments);
-    });
+    const unsubscribe = onSnapshot(
+      q,
+      (snapshot: QuerySnapshot<DocumentData>) => {
+        const allComments: Object[] = [];
+
+        snapshot.forEach((doc: any) => {
+          const data: Object = doc.data();
+          allComments.push(data);
+        });
+
+        setComments(allComments);
+      }
+    );
     return unsubscribe;
-    // eslint-disable-next-line
   }, []);
 
-  const postComment = (author, comment) => {
+  const postComment = (author: string, comment: string) => {
     return addDoc(commentsCollection, { author, comment, videoId });
   };
 
   return [comments, postComment];
-};
+}
